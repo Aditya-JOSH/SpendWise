7cda7c7484327960a2e8352f67fccc5cb42fe81c
Amelia <alsylvester@hotmail.com>
2025-10-04 11:47:27 -0700

    Add: Created website theme toggle feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+import ThemeToggle from './components/ThemeToggle';
 
 function App() {
+
+  useEffect(() => {
+    document.documentElement.classList.remove('theme-loading');
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App theme-transition">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+      <ThemeToggle />
+    </header>
+  </div>
   );
 }
 
