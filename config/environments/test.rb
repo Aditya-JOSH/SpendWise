b397eb6527373dd151d227ec448fbe515f621080
Shraddha Rao <shraddharaom@gmail.com>
2025-10-17 00:23:13 -0500

    fixed devise config issues

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # Set a dummy secret key base for test environment
+  config.secret_key_base = 'test_secret_key_base_for_testing_only'
 end
